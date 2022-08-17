66329190caf810afbaac8c0061cdfbc44c091265
Kader1680 <ouldhenniabaghdad@gmail.com>
2022-08-17 19:36:46 +0100

    code

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,12 @@
-// let box = document.querySelector('.box')
-// box.addEventListener('click', ()=>{
-//     box.style.color= "red",
-//     box.setAttribute('background-color', 'red')
-// }
+let app = document.querySelectorAll('.app')
+let el = document.querySelector('oo')
+let active = document.querySelector('.active')
 
-
-// scroll element part
-
-// let height_window = window.innerHeight;
-// console.log(height_window)
-
-window.onscroll = ()=>{
-    let skill = document.querySelector('.suqare');
-    let windowheight = window.innerHeight; // height of windows
-    let skillTop = skill.offsetTop; // top from element
-    let skillheight = skill.offsetHeight; // height element
-    let windows_scrolltop = window.pageYOffset;
-
-
-    if (windows_scrolltop > skillheight + skillTop - windowheight) {
-        // get all my skill
-
-        let TechSkill = document.querySelectorAll('.global div');
-
-        TechSkill.forEach(div => {
-
-            // div.style.display = div.dataset.display
-            alert('rf')
-
-        });
-    }
-}
-    
-    
-
-})
-
+app.forEach(li => {
+    li.addEventListener('click', ()=>{
+        el.classList.add('active')
+    })
+});
 
 
 
